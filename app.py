c21d847b8cfc93b62daf0c1addeb6232a4131e95
Sheikh Hussain <sheikh.hussain1155@gmail.com>
2026-05-28 23:34:50 +0100

    made some edits and did use the help of chatGPT to create the login required function which has now come up a few times while developing web applications

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import render_template, redirect, url_for, Flask
+from flask import render_template, redirect, url_for, Flask, session, request
 import wtforms
 import werkzeug
 import sys
 import os
 from datetime import datetime
+from helpers.auth import login_required
 
 app = Flask(__name__)
 
@@ -12,3 +13,17 @@ app = Flask(__name__)
 def home():
     title = "Roadmap.sh Blog"
     return render_template("pages/home.html", title=title)
+
+
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    title = "Admin Login"
+    if request.method == "POST":
+        session["admin_id"] = 1
+    return render_template("admin.html", title=title)
+
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    title = "Admin Dashboard"
