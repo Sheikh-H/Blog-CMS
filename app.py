c21a9d55d7245a70fd7acf6c8658ea99856b145c
Sheikh Hussain <sheikh.hussain1155@gmail.com>
2026-05-29 22:40:31 +0100

    Made some updates to the templates and files. I did want to use session configuration, ssecret keys, flask forms and etc to try and make a securer website but since this will only be deployed on a local environment I'd rather put my efforts in making something with what I already know than have to rely on ChatGPT or online sources just yet. However, I do still like the idea of using flask forms because of it's ability to prvide CSRF protection which is a nice security feature/function. What I may do is, make the first version of it without this and them embed a more modern and authentic backend design.

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import render_template, redirect, url_for, Flask, session, request
-import wtforms
 import werkzeug
 import sys
 import os
 from datetime import datetime
-from helpers.auth import login_required
+from helpers.auth import login_required, login_function
+import secrets
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
 
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -20,7 +25,15 @@ def home():
 def admin():
     title = "Admin Login"
     if request.method == "POST":
-        session["admin_id"] = 1
+        username = request.form.get("username")
+        password = request.form.get("password")
+        login = login_function(username, password)
+        if login == True:
+            session["admin_id"] = 1
+            return redirect(url_for("dashboard"))
+        else:
+            return redirect(url_for("admin"))
+
     return render_template("pages/admin/admin.html", title=title)
 
 
@@ -28,4 +41,4 @@ def admin():
 @login_required
 def dashboard():
     title = "Admin Dashboard"
-    return render_template("pages/admin/dashboard.html", title = title)
+    return render_template("pages/admin/dashboard.html", title=title)
